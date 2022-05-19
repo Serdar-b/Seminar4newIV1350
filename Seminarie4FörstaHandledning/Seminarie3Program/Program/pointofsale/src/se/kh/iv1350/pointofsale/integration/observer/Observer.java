55d658a8c43f4cbcafe11284264d59dd2eb62b08
Serdar-b <serdar-bulun@outlook.com>
2022-05-19 22:27:54 +0200

    changes

---
+++ Seminarie4FörstaHandledning/Seminarie3Program/Program/pointofsale/src/se/kh/iv1350/pointofsale/integration/observer/Observer.java
@@ -0,0 +1,10 @@
+package se.kh.iv1350.pointofsale.integration.observer;
+
+/**
+ * This is the observer interface according to the observer pattern
+ */
+public interface Observer {
+
+    void update (int totalRevenue);
+
+}
